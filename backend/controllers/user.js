9e66a9b41861a49858a1fda2948236ac906e6525
Marvin <marvinnrn@gmail.com>
2022-04-08 14:04:13 +0200

    installation jsonwebtoken

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,5 +1,7 @@
 //installation du package bcrypt pour hasher les mdp
 const bcrypt = require('bcrypt');
+//installation du package jsonwebtoken pour creer des tokens et de les vérifier
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -20,26 +22,30 @@ exports.signup = (req, res, next) => {
 
 exports.login = (req, res, next) => {
     //on récupère le user dans la base correspondant au mail entré
-    User.findOne({email: req.body.email})
-    .then(user =>{
-        //renvoie une erreur si le user n'est pas bon
-        if(!user){
-            return res.status(401).json({message: 'Utilisateur non trouvé'})
-        }
-        // si l'utilisateur est trouvé, on va comparer les mdp hashés
-        bcrypt.compare(req.body.password, user.password)
-        .then(valid => {
-            // si comparaison n'est pas bonne, on renvoi une erreur
-            if(!valid){
-                return res.status(401).json({message: 'Mot de passe invalide'})
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            //renvoie une erreur si le user n'est pas bon
+            if (!user) {
+                return res.status(401).json({ message: 'Utilisateur non trouvé' })
             }
-            // si comparaison est bonne, on renvoi son userId et un token d'authentification
-            res.status(200).json({
-                userId: user._id,
-                token: 'TOKEN'
-            });
+            // si l'utilisateur est trouvé, on va comparer les mdp hashés
+            bcrypt.compare(req.body.password, user.password)
+                .then(valid => {
+                    // si comparaison n'est pas bonne, on renvoi une erreur
+                    if (!valid) {
+                        return res.status(401).json({ message: 'Mot de passe invalide' })
+                    }
+                    // si comparaison est bonne, on renvoi son userId et un token d'authentification
+                    res.status(200).json({
+                        userId: user._id,
+                        token: jwt.sign(
+                            { userId: user._id },
+                            'RANDOM_TOKEN_SECRET',
+                            { expiresIn: '24h' }
+                        )
+                    });
+                })
+                .catch(error => res.status(500).json({ error }));
         })
-        .catch(error => res.status(500).json({error}));
-    })
-    .catch(error => res.status(500).json({error}));
+        .catch(error => res.status(500).json({ error }));
 };
