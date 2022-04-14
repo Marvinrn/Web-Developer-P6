9969bec37fde47a2773a37655ab52a2106fd6fb0
Marvin <marvinnrn@gmail.com>
2022-04-14 11:36:58 +0200

    ajout de middleware d'authentification et de multer

--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const auth = require('../middleware/auth')
+
 router.use((req, res, next) =>{
     res.json({message: 'Votre reqête a bien été reçue'})
 });
